baab52345a6b6efc422270e1e9d21fc05ca00625
Vihanga Heshan Bandara <heshanvihanga321@gmail.com>
2024-12-11 11:16:24 +0530

    HTTP Request Methods

--- Java EE/HelloServlet/src/HelloServlet.java
+++ Java EE/HelloServlet/src/HelloServlet.java
@@ -11,9 +11,37 @@ import java.io.PrintWriter;
 @WebServlet(urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("POST Request is received");
+        PrintWriter out = resp.getWriter();
+        out.println("POST Method Executed");
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("OPTIONS Request is received");
+        PrintWriter out = resp.getWriter();
+        out.println("OPTIONS Method Executed");
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("PUT Request is received");
+        PrintWriter out = resp.getWriter();
+        out.println("PUT Method Executed");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("DELETE Request is received");
+        PrintWriter out = resp.getWriter();
+        out.println("DELETE Method Executed");
+    }
+
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            System.out.println("Request is received");
+            System.out.println("GET Request is received");
             PrintWriter out = resp.getWriter();
-            out.println("Hello World!");
+            out.println("GET Method Executed");
         }
 }
