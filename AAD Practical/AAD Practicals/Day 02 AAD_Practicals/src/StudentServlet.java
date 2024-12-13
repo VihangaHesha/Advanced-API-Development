21a8be9dbec014f585c32731f85451449591baa1
Vihanga Heshan Bandara <heshanvihanga321@gmail.com>
2024-12-13 14:28:46 +0530

    Done the delete and the put methods

--- AAD Practical/AAD Practicals/Day 02 AAD_Practicals/src/StudentServlet.java
+++ AAD Practical/AAD Practicals/Day 02 AAD_Practicals/src/StudentServlet.java
@@ -26,7 +26,6 @@ public class StudentServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
-
         resp.setContentType("application/json");
 
         StringBuilder studentJsonList = new StringBuilder("[");
@@ -48,6 +47,70 @@ public class StudentServlet extends HttpServlet {
     }
 
     @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String age = req.getParameter("age");
+
+        if (id == null || id.isEmpty()) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write("\"{\"error\": \"Id is required!\"}");
+        }
+        try{
+            StudentDTO studentDTO = findById(Integer.parseInt(id));
+                if (studentDTO == null) {
+                    resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    resp.getWriter().write("{\"error\": \"Student not found!\"}");
+                }else {
+                    studentDTO.setName(name);
+                    studentDTO.setEmail(email);
+                    studentDTO.setAge(Integer.parseInt(age));
+                    resp.setStatus(HttpServletResponse.SC_CREATED);
+                    resp.getWriter().write("{\"message\": \"Student updated successfully\"}");
+                }
+            }catch (NumberFormatException e){
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                resp.getWriter().write("{\"error\": \"Invalid Id\"}");
+            }
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        if (id == null || id.isEmpty()) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write("{\"error\": \"Id is required!\"}");
+        } else {
+            try {
+                int idInteger = Integer.parseInt(id);
+                StudentDTO studentDTO = findById(idInteger);
+                if (studentDTO == null) {
+                    resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    resp.getWriter().write("{\"error\": \"Student not found!\"}");
+                } else {
+                    studentDTOList.remove(studentDTO);
+                    resp.getWriter().write("{ \"message\": \"Student Deleted Successfully\"}");
+                }
+            } catch (NumberFormatException e) {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                resp.getWriter().write("{ \"error\": \"Invalid Id\"}");
+            }
+        }
+    }
+
+    private StudentDTO findById(int id) {
+    for (StudentDTO studentDTO : studentDTOList){
+        if (studentDTO.getId() == id){
+            return studentDTO;
+        }
+    }
+        return null;
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
         String email = req.getParameter("email");
@@ -57,6 +120,7 @@ public class StudentServlet extends HttpServlet {
         try{
             age = Integer.parseInt(ageString);
             id = studentDTOList.size() + 1;
+
         }catch (NumberFormatException e){
             System.out.println("Invalid Age!!");
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
@@ -64,7 +128,14 @@ public class StudentServlet extends HttpServlet {
             return;
         }
 
-        StudentDTO studentDTO = new StudentDTO(id, name, email, age);
-        studentDTOList.add(studentDTO);
+        if (name.isEmpty() || name == null || email.isEmpty() || email == null || ageString == null) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write("{\"error\": \"Invalid Input.Name,Emall and Age cannot be empty\"}");
+
+        }else {
+            StudentDTO studentDTO = new StudentDTO(id, name, email, age);
+            studentDTOList.add(studentDTO);
+            resp.setStatus(HttpServletResponse.SC_CREATED);
+        }
     }
 }
