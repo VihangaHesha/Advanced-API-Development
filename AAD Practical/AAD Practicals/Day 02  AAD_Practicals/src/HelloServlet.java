3f0d1899abdfd350ff0078f79230dab992e25b7a
Vihanga Heshan Bandara <heshanvihanga321@gmail.com>
2024-12-13 09:08:32 +0530

    Initiated the JavaEE project and configured the Tomcat Server into the Project

--- AAD Practical/AAD Practicals/Day 02  AAD_Practicals/src/HelloServlet.java
+++ AAD Practical/AAD Practicals/Day 02  AAD_Practicals/src/HelloServlet.java
@@ -7,11 +7,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/hello")
+@WebServlet (urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Request is received");
+        System.out.println("Request Received");
         PrintWriter out = resp.getWriter();
         out.println("Hello World!");
     }
