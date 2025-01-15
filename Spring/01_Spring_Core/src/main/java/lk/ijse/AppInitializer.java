1cb3d3d6fec90fc27003772674fa4408c1e26e50
Vihanga Heshan Bandara <heshanvihanga321@gmail.com>
2025-01-15 11:40:40 +0530

    Tested the object creation in spring

--- Spring/01_Spring_Core/src/main/java/lk/ijse/AppInitializer.java
+++ Spring/01_Spring_Core/src/main/java/lk/ijse/AppInitializer.java
@@ -1,5 +1,23 @@
 package lk.ijse;
 
-public class AppInitializer {
+import lk.ijse.bean.SpringBean;
+import lk.ijse.bean.TestBean1;
+import lk.ijse.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
+
+
+        SpringBean bean = context.getBean(SpringBean.class);
+        System.out.println(bean);
+        bean.testBean();
+        TestBean1 testBean1 = context.getBean(TestBean1.class);
+        System.out.println(testBean1);
+
+        context.close();
+    }
 }
