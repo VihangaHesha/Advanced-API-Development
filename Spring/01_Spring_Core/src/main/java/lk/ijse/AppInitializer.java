9fb7afb766a353ef6dbc7ea219eec811dd8277da
Vihanga Heshan Bandara <heshanvihanga321@gmail.com>
2025-01-15 14:23:38 +0530

    Added new beans and done some customizations in the bean Ids

--- Spring/01_Spring_Core/src/main/java/lk/ijse/AppInitializer.java
+++ Spring/01_Spring_Core/src/main/java/lk/ijse/AppInitializer.java
@@ -1,7 +1,9 @@
 package lk.ijse;
 
+import lk.ijse.bean.MyConnection;
 import lk.ijse.bean.SpringBean;
 import lk.ijse.bean.TestBean1;
+import lk.ijse.bean.TestBean2;
 import lk.ijse.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,13 +13,21 @@ public class AppInitializer {
         context.register(AppConfig.class);
         context.refresh();
 
-
-        SpringBean bean = context.getBean(SpringBean.class);
+      /*  Object bean = context.getBean("springBean");
         System.out.println(bean);
-        bean.testBean();
-        TestBean1 testBean1 = context.getBean(TestBean1.class);
+
+        TestBean1 testBean1 = context.getBean("testBean1",TestBean1.class);
         System.out.println(testBean1);
 
-        context.close();
+        TestBean2 testBean2 = context.getBean(TestBean2.class);
+        System.out.println(testBean2);
+
+        MyConnection myConnectionBean = context.getBean(MyConnection.class);
+        System.out.println(myConnectionBean);
+
+        MyConnection myConnectionBean1 = (MyConnection) context.getBean("getConnection");
+        System.out.println(myConnectionBean1);*/
+
+        context.registerShutdownHook();
     }
 }
--- Spring/01_Spring_Core/src/main/java/lk/ijse/config/AppConfig.java
+++ Spring/01_Spring_Core/src/main/java/lk/ijse/config/AppConfig.java
@@ -1,9 +1,16 @@
 package lk.ijse.config;
 
+import lk.ijse.bean.MyConnection;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.bean")
 public class AppConfig {
+
+    @Bean("connection")//customized bean id
+    MyConnection getConnection(){
+        return new MyConnection();
+    }
 }
