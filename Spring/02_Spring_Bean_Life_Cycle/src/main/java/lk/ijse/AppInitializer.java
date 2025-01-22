5dabc0f91cdeea39b91addb153c69f734f1b0758
Vihanga Heshan Bandara <heshanvihanga321@gmail.com>
2025-01-22 09:50:12 +0530

    Built a new module and configured for Spring and added classes : SpringBean,AppConfig and AppInitializer.

---
+++ Spring/02_Spring_Bean_Life_Cycle/src/main/java/lk/ijse/AppInitializer.java
@@ -0,0 +1,21 @@
+package lk.ijse;
+
+import lk.ijse.bean.SpringBean;
+import lk.ijse.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
+
+        SpringBean bean1 = context.getBean(SpringBean.class);
+        SpringBean bean2 = context.getBean(SpringBean.class);
+
+        System.out.println(bean1);
+        System.out.println(bean2);
+
+        context.registerShutdownHook();
+    }
+}
---
+++ Spring/02_Spring_Bean_Life_Cycle/src/main/java/lk/ijse/config/AppConfig.java
@@ -0,0 +1,15 @@
+package lk.ijse.config;
+
+import lk.ijse.bean.SpringBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.bean")
+public class AppConfig {
+    @Bean("connection")//customized bean id
+    SpringBean getSpringBean(){
+        return new SpringBean();
+    }
+}
